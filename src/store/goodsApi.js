b8824db8aed35583d16fb76c663ecbb195ecd925
salvation27 <nikolayzalipykin@gmail.com>
2022-03-14 15:16:26 +0200

    добавление товар собновлением списка

--- src/store/goodsApi.js
+++ src/store/goodsApi.js
@@ -2,19 +2,27 @@ import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 export const goodsApi = createApi({
     reducerPath:'goodsApi',
+    tegTypes:['Produccts'],
     baseQuery:fetchBaseQuery({
         baseUrl:'https://622f13593ff58f023c150843.mockapi.io/test/'
     }),
     endpoints:(build)=>({
         getGoods:build.query({
             query: (limit = '') => `shop1${limit && `?page=1&limit=${limit}`}`,
+            providesTags: (result) =>
+            result
+              ? [...result.map(({ id }) => ({ type: 'Produccts', id })), {type:'Produccts',id:'LIST'}]
+              : [{type:'Produccts',id:'LIST'}],
         }),
         addProduct:build.mutation({
             query: (body)=> ({
                 url:'shop1',
                 method:'POST',
                 body,
-            })
+            }),
+            invalidatesTags:[{
+                type:'Produccts',id:'LIST'
+            }]
         }),
     })
     
